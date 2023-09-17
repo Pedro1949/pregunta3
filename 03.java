96cdddf73c9d372e62e57f51fa6067624705144a
Pedro1949 <142362192+Pedro1949@users.noreply.github.com>
2023-09-16 23:00:22 -0500

    Add files via upload

---
+++ 03.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        double euros = 32; 
+
+        
+        double tasaDeCambio = 156.34;
+
+        
+        double pesetas = euros * tasaDeCambio;
+
+        
+        System.out.println("Se tendra: "+pesetas + " pesetas.");
+    }
+}
